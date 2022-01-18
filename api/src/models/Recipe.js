1707fc65e21dfc7e3bee18e2da46b72bb3e3d151
Jorgelopez69773 <jorgelopez.trabajo@gmail.com>
2022-01-18 13:48:29 -0300

    database complete

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -4,9 +4,31 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('recipe', {
+    id:{
+      type:DataTypes.UUID,
+      defaultValue:DataTypes.UUIDV4,
+      primaryKey:true,
+      allowNull:false,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    summary:{
+      type:DataTypes.TEXT,
+      allowNull:false
+    },
+    //PUNTAJE
+    spoonacularScore:{
+      type:DataTypes.INTEGER,
+    },
+    //NIVEL DE SALUD:
+    healthScore:{
+      type:DataTypes.INTEGER,
+    },
+    //PASO A PASO:
+    analyzedInstructions:{
+      type:DataTypes.ARRAY(DataTypes.TEXT),
+    }
   });
 };
